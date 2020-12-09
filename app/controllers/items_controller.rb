7795a3ac259ffad450b33e4233fe46f0758c3fe4
tatsu98s <73688529+tatsu98s@users.noreply.github.com>
2020-12-09 14:46:04 +0900

    商品詳細表示機能修正４

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
   before_action :set_item, only: [:edit, :show]
 
   def index
